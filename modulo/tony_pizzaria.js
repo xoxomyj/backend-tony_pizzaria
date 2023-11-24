51aa8384edf0fab4cd205e816d840b0203361979
MARIANA ALVES DE SOUSA <23111755@CFP127ALUNO.LOCAL>
2023-11-24 12:18:25 -0300

    em andamento

---
+++ modulo/tony_pizzaria.js
@@ -0,0 +1,121 @@
+var Usuarios = {
+    login: 'Usuario'
+    [
+        {
+            nome : 'Mariana Alves de Sousa',
+            email : 'mariana@gmail.com',
+            telefone: '+11 966197988',
+            senha: '123peetateamo',
+            imagem: '',
+            endereco: 'Rua Primrose, Distrito 04, N-0210'
+        },
+        {
+            nome : 'Gustavo Henrique de Lima Santos',
+            email : 'gustavo@gmail.com',
+            telefone: '+11 966197989',
+            senha: '123jogosvorazes',
+            imagem: '',
+            endereco: 'Rua Tordo, Distrito 12, N-1209'
+        },
+        {
+            nome : 'Bianca Maria Soares',
+            email : 'bianca@gmail.com',
+            telefone: '+11 966197990',
+            senha: '123finnickteamo',
+            imagem: '',
+            endereco: 'Rua Katniss, Distrito 01, N-2712'
+        }
+    ]
+}
+
+var Categorias = {
+    area: 'Categorias' 
+    [
+        {
+            nome : 'Pizzas Salgada',
+            imagem: '',
+            descricao: 'Seção dedicada para nossos sabores de Pizzas Sagaldas.'   
+         },
+         {
+            nome : 'Bebidas',
+            imagem: '',
+            descricao: 'Seçâo dedicada para Bebidas em geral.'
+         },
+         {
+            nome : 'Frutas',
+            imagem: '',
+            descricao: 'Seção dedicada para nossas saborosas Frutas.'   
+         },
+         {
+            nome : 'Pizzas Doces',
+            imagem: '',
+            descricao: 'Seção dedicada para nossos sabores de Pizzaz Doces.'   
+         },
+         {
+            nome : 'Sobremesas',
+            imagem: '',
+            descricao: 'Seção dedicada para nossas diversas Sobremesas.'   
+         },
+         {
+            nome : 'Sorvetes',
+            imagem: '',
+            descricao: 'Seção dedicada para nossos nossos sabores de Sorvetes.'   
+         }
+
+    ]
+}
+
+var Produtos  = {
+    area: 'Produtos'
+    [
+        {
+            nome : '',
+            imagem: '',
+            descricao: '',
+            valores: '',
+            avaliacao: ''
+        },
+        {
+            nome : '',
+            imagem: '',
+            descricao: '',
+            valores: '',
+            avaliacao: ''
+        },
+        {
+            nome : '',
+            imagem: '',
+            descricao: '',
+            valores: '',
+            avaliacao: ''
+        },
+        {
+            nome : '',
+            imagem: '',
+            descricao: '',
+            valores: '',
+            avaliacao: ''
+        },
+        {
+            nome : '',
+            imagem: '',
+            descricao: '',
+            valores: '',
+            avaliacao: ''
+        },
+        {
+            nome : '',
+            imagem: '',
+            descricao: '',
+            valores: '',
+            avaliacao: ''
+        },
+        {
+            nome : '',
+            imagem: '',
+            descricao: '',
+            valores: '',
+            avaliacao: ''
+        }
+    ]
+};
